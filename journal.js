c71ab987f6f05839d694997e90c1ff0f5cdf74f0
Baptiste-Williams <tc1028@hotmail.com>
2025-06-30 19:05:51 -0700

    I add 4 more basic functions to journal.js

--- journal.js
+++ journal.js
@@ -1,4 +1,26 @@
-// Returns a greeting message
+// 1. Returns a greeting message
 function greet(name) {
   return "Hello, " + name + "!";
 }
+
+// 2. Adds two numbers
+function add(a, b) {
+  return a + b;
+}
+
+// 3. Checks if a number is even
+function isEven(num) {
+  return num % 2 === 0;
+}
+
+// 4. Converts Celsius to fahrenheit 
+function toFahrenheit(celsius) {
+  return (celsius * 9/5) + 32;
+}
+
+// 5. Returns the length of a string
+function getLength(str) {
+  return str. length;
+}
+
+
